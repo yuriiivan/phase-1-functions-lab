d3bef89e10fae1d042d80a7cbb960f58c4485b95
Yuriy Ivanenko <yuriy.ivanenko@powerhrg.com>
2024-05-06 11:56:48 -0400

    Passed all tests

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(destination){
+    if(destination > 42){
+        return destination - 42
+    }
+    else{
+        return 42 - destination
+    }
+}
+
+function distanceFromHqInFeet(destination){
+    if(destination > 42){
+        return (destination - 42) * 264
+    }
+    else{
+        return (42 - destination) * 264
+    }
+}
+
+function distanceTravelledInFeet(start, destination){
+    if(start > destination){
+        return (start - destination) * 264
+    } else{
+        return (destination - start) * 264
+    }
+}
+
+function calculatesFarePrice(start, destination){
+    //Destination in feet
+    const distance = start > destination ? (start - destination) * 264 : (destination - start) * 264
+    if(distance <= 400 ){
+        return 0;
+    }else if(distance > 400 && distance < 2000){
+        return .02 * (distance-400);
+    }else if(distance > 2500){
+        return 'cannot travel that far'
+    }else if(distance > 2000){
+        return 25;
+    }
+}
