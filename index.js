5e8502ea6181b6a686bcf0286791f86ba4776334
Yuriy Ivanenko <yuriy.ivanenko@powerhrg.com>
2024-05-07 11:14:05 -0400

    Refactor functions

--- index.js
+++ index.js
@@ -1,40 +1,27 @@
 // Code your solution in this file!
 function distanceFromHqInBlocks(destination){
-    if(destination > 42){
-        return destination - 42
-    }
-    else{
-        return 42 - destination
-    }
+    return Math.abs(destination - 42)
 }
 
 function distanceFromHqInFeet(destination){
-    if(destination > 42){
-        return (destination - 42) * 264
-    }
-    else{
-        return (42 - destination) * 264
-    }
+    return Math.abs(distanceFromHqInBlocks(destination) * 264)
 }
 
 function distanceTravelledInFeet(start, destination){
-    if(start > destination){
-        return (start - destination) * 264
-    } else{
-        return (destination - start) * 264
-    }
+    return Math.abs((start - destination) * 264)
 }
 
 function calculatesFarePrice(start, destination){
     //Destination in feet
-    const distance = start > destination ? (start - destination) * 264 : (destination - start) * 264
+    // const distance = start > destination ? (start - destination) * 264 : (destination - start) * 264
+    const distance = distanceTravelledInFeet(start, destination)
     if(distance <= 400 ){
         return 0;
-    }else if(distance > 400 && distance < 2000){
-        return .02 * (distance-400);
-    }else if(distance > 2500){
-        return 'cannot travel that far'
-    }else if(distance > 2000){
+    }else if(distance < 2000){
+        return .02 * (distance - 400);
+    }else if(distance < 2500){
         return 25;
+    }else{
+        return 'cannot travel that far'
     }
 }
